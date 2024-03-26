bcf4b0ff991ded6c0829386d3878a66e5b25265c
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-03-26 23:36:35 +0200

    set tailwind, initial components, api and prisma

---
+++ app/about/info/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutInfoPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">AboutInfo Page</h1>
+    </div>
+  );
+};
+
+export default AboutInfoPage;
---
+++ app/query/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QueryPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">Query Page</h1>
+    </div>
+  );
+};
+
+export default QueryPage;
