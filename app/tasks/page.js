ffb53471602a58b45fcb04f4c1bd1bce92be90a3
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-03-27 23:36:17 +0200

    set up prisma, db and components

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,9 +1,14 @@
+import TaskForm from "@/components/TaskForm";
+import TaskList from "@/components/TaskList";
 import React from "react";
 
 const TasksPage = () => {
   return (
     <div>
-      <h1 className="text-7xl">TasksPage</h1>
+      <h1 className="text-7xl">
+        <TaskForm />
+        <TaskList />
+      </h1>
     </div>
   );
 };
