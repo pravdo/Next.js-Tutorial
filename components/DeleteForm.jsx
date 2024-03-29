bc9594faf65de3ce44096b7324a5e741e0f127d8
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-03-29 17:34:57 +0200

    api with thunder client test

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,11 +1,22 @@
+"use client";
 import React from "react";
 import { deleteTask } from "@/utils/actions";
+import { useFormStatus } from "react-dom";
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button className="btn btn-xs btn-error" disabled={pending}>
+      {pending ? "pending..." : "delete"}
+    </button>
+  );
+};
 
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type="hidden" name="id" value={id} />
-      <button className="btn btn-error btn-xs">delete</button>
+      <SubmitButton />
     </form>
   );
 };
